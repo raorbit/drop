d5cc95da521d3a7a079ef3a63785b2e6317418a7
raorbit <raorbit@gmail.com>
2015-05-13 23:41:08 -0400

    Initial commit

---
+++ android/src/com/admala/drop/android/AndroidLauncher.java
@@ -0,0 +1,19 @@
+package com.admala.drop.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.admala.drop.Drop;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useAccelerometer = false;
+		config.useCompass = false;
+		config.useImmersiveMode = true;
+		initialize(new Drop(), config);
+	}
+}
